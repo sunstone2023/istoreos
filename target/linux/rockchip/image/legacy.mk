8f43e223a34984fbd8c344509c8d2e6c85b8b33d
Liangbin Lian <jjm2473@gmail.com>
2025-03-14 23:08:44 +0800

    target/rockchip: friendlyarm nanopi r6s/r6c use extended dts
    Signed-off-by: Liangbin Lian <jjm2473@gmail.com>

--- target/linux/rockchip/image/legacy.mk
+++ target/linux/rockchip/image/legacy.mk
@@ -10,9 +10,17 @@ $(call Device/Legacy/rk3588s)
   DEVICE_MODEL := NanoPi R6S/R6C
   SUPPORTED_DEVICES += friendlyarm,nanopi-r6s friendlyarm,nanopi-r6c
   SUPPORTED_DEVICES += friendlyelec,nanopi-r6s friendlyelec,nanopi-r6c
-  DEVICE_DTS := rockchip/rk3588s-nanopi-r6s rockchip/rk3588s-nanopi-r6c
+  DEVICE_DTS := rk3588/rk3588s-nanopi-r6s rk3588/rk3588s-nanopi-r6c
   BOOT_SCRIPT := rk3588-friendlyelec
   UBOOT_DEVICE_NAME := easepi-rk3588
   DEVICE_PACKAGES += kmod-r8169
+endef
+
+# avoid using upstream dts
+define Device/friendlyarm_nanopi-r6c
+$(call Device/Legacy/rk3588s)
+  DEVICE_VENDOR := FriendlyARM
+  DEVICE_MODEL := NanoPi R6C
+  DEVICE_DTS := rk3588/rk3588s-nanopi-r6c
   DEVICE_PACKAGES := kmod-r8169
 endef
